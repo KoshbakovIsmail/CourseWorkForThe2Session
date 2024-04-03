ddae3464b4ab4a84db85000726287ccdb3f5f5f6
KosbakovIsmail <kosbakovismail@gmail.com>
2024-04-03 12:08:32 +0300

    commit 1

---
+++ demoCourseWorkForThe2Session/src/main/java/com/WorkCourse/demoCourseWorkForThe2Session/DemoCourseWorkForThe2SessionApplication.java
@@ -0,0 +1,13 @@
+package com.WorkCourse.demoCourseWorkForThe2Session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCourseWorkForThe2SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCourseWorkForThe2SessionApplication.class, args);
+	}
+
+}
---
+++ demoCourseWorkForThe2Session/src/test/java/com/WorkCourse/demoCourseWorkForThe2Session/DemoCourseWorkForThe2SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WorkCourse.demoCourseWorkForThe2Session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCourseWorkForThe2SessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
